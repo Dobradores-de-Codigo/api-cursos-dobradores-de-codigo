42113a60a02e263108f4228c0dbd7dd14788deaa
Vagnerflj <116043257+vagnerflj@users.noreply.github.com>
2024-05-08 15:44:01 -0300

    Implementado classe CustomException excecao personalizada

---
+++ src/main/java/com/example/cursos/exception/CustomException.java
@@ -0,0 +1,16 @@
+package com.example.cursos.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+public class CustomException extends RuntimeException {
+
+    public CustomException(String message) {
+        super(message);
+    }
+
+    public CustomException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
