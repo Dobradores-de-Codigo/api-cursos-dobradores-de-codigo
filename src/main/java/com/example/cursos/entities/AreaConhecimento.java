e2aad6a7614d31acd1a6d7ae5244d2246961f9d6
Jeferson5641 <jefe.gomes154@gmail.com>
2024-05-08 19:40:38 -0300

    Implementação do enum AreaConhecimento

---
+++ src/main/java/com/example/cursos/entities/AreaConhecimento.java
@@ -0,0 +1,14 @@
+package com.example.cursos.entities;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public enum AreaConhecimento {
+
+    TI,
+    MARKETING,
+    FINANÇAS,
+    RH,
+    OUTROS
+
+}
